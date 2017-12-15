10bc7745e824a13d42d2621229da22c5e7315fc3
danieldrn <daniel.nascimento@escolarmanager.com.br>
2017-12-15 14:42:32 -0200

    M0015 - Exclui projeto controles

---
+++ CORUJAMANAGER/CORUJAMANAGER/Controles/ControleDePesquisa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CORUJAMANAGER.Controles
+{
+    public partial class ControleDePesquisa : UserControl
+    {
+        public ControleDePesquisa()
+        {
+            InitializeComponent();
+        }
+    }
+}
